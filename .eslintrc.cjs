3ddcc036ddbeaa0204c592c1cd17340c81de0616
KennetTech <64315533+KennetTech@users.noreply.github.com>
2022-12-14 17:55:19 +0100

    change Eslint and generated new report

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,13 @@
 module.exports = {
+    "plugins": [
+        "jest",
+        "cypress"
+    ],
     "env": {
         "browser": true,
         "es2021": true,
-        "jest/globals": true
+        "jest/globals": true,
+        "cypress/globals": true
     },
     "extends": "eslint:recommended",
     "overrides": [
@@ -12,5 +17,16 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error",
+        "cypress/no-assigning-return-values": "error",
+        "cypress/no-unnecessary-waiting": "error",
+        "cypress/assertion-before-screenshot": "warn",
+        "cypress/no-force": "warn",
+        "cypress/no-async-tests": "error",
+        "cypress/no-pause": "error"
     }
 }
